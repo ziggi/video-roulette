f5c63ae8c6433a8cfc1439ec41efc81557ce9cd2
ziggi <xziggix@gmail.com>
2015-06-01 00:19:21 +0300

    add VideoStream lib (fix webm header bugs)

--- bootstrap.php
+++ bootstrap.php
@@ -12,9 +12,10 @@ $file_path = 'file/' . $file_name[0] . '/' . $file_name[1] . '/' . $file_name;
 $is_valid_file = preg_match('/^[0-9a-f]{32}\.[a-z]+$/', $file_name) == 1;
 
 if ($is_valid_file && file_exists($file_path)) {
-	header('Content-type: video/webm');
-
-	readfile($file_path);
+	include 'VideoStream.php';
+	
+	$stream = new VideoStream($file_path);
+	$stream->start();
 } else {
 	header('HTTP/1.0 404 Not Found');
 	echo '404';
